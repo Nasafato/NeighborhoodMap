49d2bbc408266569c449304fd1355712b1ee7e9b
alan-kinnek <alan@kinnek.com>
2016-04-14 14:01:49 -0400

    Got basic layout done

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta charset="utf-8">
+
+<title>Neighborhood Map</title>
+<meta name="description" content="Neighborhood Map using KnockJS">
+<meta name="author" content="Alan Gou">
+
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<script src="js/scripts.js"></script>
+</body>
+</html>

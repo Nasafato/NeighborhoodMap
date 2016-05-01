4af7289fb19bc6cc2fdc34077e013157bd045f9a
alan-kinnek <alan@kinnek.com>
2016-05-01 01:31:38 -0400

    Can now toggle on and off the appearance of markers and their info windows

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,79 @@
 "use strict"
 
 var toggled = false;
+var map;
 
-console.log(window.innerWidth);
-
-var Location = function(lat, long) {
+var Location = function(lat, long, title) {
 	var self = this;
 
-	this.lat = lat;
-	this.long = long;
+	this.title = title;
 	this.isVisible = ko.observable(true);
+	this.createMarker = function() {
+		var latLong = new google.maps.LatLng(lat, long);
+		var newMarker = new google.maps.Marker({
+			position: latLong,
+			title: title
+		});
+        var infoWindowHTML = '<b>'+title+'</b><hr>';
+		var infoWindow = new google.maps.InfoWindow({
+		 	content: infoWindowHTML,
+            size: new google.maps.Size(150,50)
+        });
+
+        google.maps.event.addListener(newMarker, 'click', function() {
+        	infoWindow.open(map, newMarker);
+        });
+
+		return newMarker;
+	};
+
+	this.marker = this.createMarker(lat, long, title);
+	this.marker.setMap(map);
 
 	this.turnOff = function() {
 		this.isVisible(false);
+		this.marker.setMap(null)
 	};
 
 	this.turnOn = function() {
 		this.isVisible(false);
+		this.marker.setMap(map);
 	};
-
 };
 
+var modelSelf;
 var LocationListModel = function() {
+	modelSelf = this;
 	this.locations = ko.observableArray([]);
+	this.toggleOff = function() {
+		this.locations().forEach(function(location) {
+			location.turnOff();
+		});
+	}
 
-	this.addLocation = function(latitude, longitude) {
-		var newLocation = Location(latitude, longitude);
+	this.toggleOn = function() {
+		this.locations().forEach(function(location) {
+			location.turnOn();
+		});
+	}
+
+	this.addLocation = function(latitude, longitude, title) {
+		var newLocation = Location(latitude, longitude, title);
+		this.locations().push(newLocation);
 	}
 };
 
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 40.8065, lng: -73.9619},
+		scrollwheel: true,
+		zoom: 16
+	});
+	modelSelf.locations().push(new Location(40.806129, -73.965654, "Nussbaum and Wu"));
+}
 
+
+ko.applyBindings(new LocationListModel())
 
 function toggleSidebar() {
 	var sidebar = document.getElementById("sidebar-wrapper");
@@ -40,16 +84,12 @@ function toggleSidebar() {
 	if (toggled) {
 		sidebar.removeAttribute("style");
 		content.removeAttribute("style");
-		/*
-		sidebar.style.width = 0 + "%";
-		sidebar.style.position = "relative";
-		content.style.width = 100 + "%";
-		*/
 		toggled = false;
 	} else {
 		sidebar.style.display = "block";
 		sidebar.style.position = "absolute";
 		sidebar.style.width = 50 + "%"
+
 		content.style.display = "block";
 		content.style.width = 100 + "%";
 
@@ -66,18 +106,4 @@ window.onresize = function() {
 		sidebar.removeAttribute("style");
 		content.removeAttribute("style");
 	}
-
 }
-
-
-function initMap() {
-  // Create a map object and specify the DOM element for display.
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-		scrollwheel: true,
-		zoom: 8
-	});
-}
-
-
-
